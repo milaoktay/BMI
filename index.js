335a5df83a2d2a037e6c5bc11f05a1e4f902408b
Mila Oktay <59603620+milaoktay@users.noreply.github.com>
2022-07-06 14:23:50 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function run() {
-      let hohe = document.querySelector('#hohe').value;
-      let gewicht = document.querySelector('#gewicht').value;
-      hohe = hohe / 100;
-
-      const bmi = (gewicht / hohe / hohe).toFixed(1);
-      document.querySelector('#bmi').innerHTML = `Dein BMI ist ${bmi}`;
-}
